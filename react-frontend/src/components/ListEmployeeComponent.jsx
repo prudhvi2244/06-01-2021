7392a8cb67a021e4f500753b2ab0cfe8f21c3231
Raj <raj.rajeev2244@gmail.com>
2021-01-06 14:23:56 +0530

    Intial Commit

---
+++ react-frontend/src/components/ListEmployeeComponent.jsx
@@ -0,0 +1,83 @@
+import React, { Component } from 'react'
+import EmployeeService from '../services/EmployeeService'
+
+class ListEmployeeComponent extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+                employees: []
+        }
+        this.addEmployee = this.addEmployee.bind(this);
+        this.editEmployee = this.editEmployee.bind(this);
+        this.deleteEmployee = this.deleteEmployee.bind(this);
+    }
+
+    deleteEmployee(eid){
+        EmployeeService.deleteEmployee(eid).then( res => {
+            this.setState({employees: this.state.employees.filter(employee => employee.eid !== eid)});
+        });
+    }
+    viewEmployee(eid){
+        this.props.history.push(`/view-employee/${eid}`);
+    }
+    editEmployee(eid){
+        this.props.history.push(`/add-employee/${eid}`);
+    }
+
+    componentDidMount(){
+        EmployeeService.getEmployees().then((res) => {
+            this.setState({ employees: res.data});
+        });
+    }
+
+    addEmployee(){
+        this.props.history.push('/add-employee/_add');
+    }
+
+    render() {
+        return (
+            <div>
+                 <h2 className="text-center">Employees List</h2>
+                 <div className = "row">
+                    <button className="btn btn-primary" onClick={this.addEmployee}> Add Employee</button>
+                 </div>
+                 <br></br>
+                 <div className = "row">
+                        <table className = "table table-striped table-bordered">
+
+                            <thead>
+                                <tr>
+                                    <th> Employee Name</th>
+                                    <th> Employee City</th>
+                                    <th> Employee Email</th>
+                                    <th> Actions</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {
+                                    this.state.employees.map(
+                                        employee => 
+                                        <tr key = {employee.eid}>
+                                             <td> {employee.ename} </td>   
+                                             <td> {employee.ecity}</td>
+                                             <td> {employee.email}</td>
+                                             <td>
+                                                 <button onClick={ () => this.editEmployee(employee.id)} className="btn btn-info">Update </button>
+                                                 <button style={{marginLeft: "10px"}} onClick={ () => this.deleteEmployee(employee.id)} className="btn btn-danger">Delete </button>
+                                                 <button style={{marginLeft: "10px"}} onClick={ () => this.viewEmployee(employee.id)} className="btn btn-info">View </button>
+                                             </td>
+                                        </tr>
+                                    )
+                                }
+                            </tbody>
+                        </table>
+
+                 </div>
+
+            </div>
+        )
+    }
+}
+
+export default ListEmployeeComponent
